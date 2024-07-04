14b9ca00ae780911ef03c3cb5c4cb05cfeb46210
Henrique <67938666+HazelicEyes@users.noreply.github.com>
2024-07-04 07:55:17 -0300

    ninguem viu nada

--- exercicio06.java
+++ exercicio06.java
@@ -3,8 +3,15 @@ package exercicios_para_entrega;
 public class exercicio06 {
 
 	public static void main(String[] args) {
+//		String array1[] = {"a","vida","é","bela"};
+//		String texto1 = array1[0] + " " + array1[1] + " " + array1[2] + " " + array1[3];
+//		String texto2 = texto1.toUpperCase();
+//		System.out.println(texto2);
+
 		String array1[] = {"a","vida","é","bela"};
-		String texto1 = array1[0] + " " + array1[1] + " " + array1[2] + " " + array1[3];
+		String texto1 = new String();
+		for (int x = 0; x<=3; x++)
+			texto1 = texto1 + " " + array1[x];
 		String texto2 = texto1.toUpperCase();
 		System.out.println(texto2);
 
